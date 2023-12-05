1562256a1eea2cd2c62f88e04b8e060d38df78d1
MaryamStormer <maryam@brainstormertech.co.uk>
2023-12-05 10:53:51 +0500

    latest code received

--- android/app/src/main/kotlin/com/americanlyceum/chat_gpt_stories/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.americanlyceum.chat_gpt_stories
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/brainstormers/chat_gpt_stories/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.brainstormers.chat_gpt_stories
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
